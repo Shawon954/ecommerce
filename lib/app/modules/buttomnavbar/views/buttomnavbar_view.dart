d50076fa5f7d54426c4eae01f5cb74e034678ba8
shawo <shawoncse954@gmail.com>
2023-09-08 01:17:16 +0600

    buttomnavbar icon set

--- lib/app/modules/buttomnavbar/views/buttomnavbar_view.dart
+++ lib/app/modules/buttomnavbar/views/buttomnavbar_view.dart
@@ -1,58 +1,67 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ostad_ecommerce/app/utility/app_image/app_image.dart';
 import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
 
 import '../controllers/buttomnavbar_controller.dart';
 
 class ButtomnavbarView extends GetView<ButtomnavbarController> {
-  final ButtomnavbarController _buttomnavbarController = Get.put(
-      ButtomnavbarController());
+  final ButtomnavbarController _buttomnavbarController =
+      Get.put(ButtomnavbarController());
 
   ButtomnavbarView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    bottomNavigationBar: _BottomNavBar(),
-      body: const Center(
-        child: Text(
-          'ButtomnavbarView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
-  }
 
+
+    return GetBuilder<ButtomnavbarController>(
+        builder: (controller){
+
+          return Scaffold(
+            body: IndexedStack(
+              index: controller.CurrentIndex,
+              children: [
+
+              ],
+            ),
+          );
+        }
+
+
+    );
+
+  }
 
   Widget _BottomNavBar() {
     return WaterDropNavBar(
       selectedIndex: controller.CurrentIndex,
       onItemSelected: (int index) {
-          controller.CurrentIndex = index;
+        controller.CurrentIndex = index;
       },
       barItems: <BarItem>[
         BarItem(
-          filledIcon: Icons.bookmark_rounded,
-          outlinedIcon: Icons.bookmark_border_rounded,
-          Text: "",
+          Text: "Home",
+          fillImage: Image.asset(AppImage.fillbottomnavbarhomeicon,scale: 1.2,),
+          outlineImage: Image.asset(AppImage.outlinebottomnavbarhomeicon,scale: 1.2,),
         ),
         BarItem(
-            filledIcon: Icons.favorite_rounded,
-            outlinedIcon: Icons.favorite_border_rounded,
-          Text: "",
-        ),
-
-        BarItem(
-          filledIcon: Icons.email_rounded,
-          outlinedIcon: Icons.email_outlined,
-          Text: "",
+          Text: "Categorys",
+          fillImage:Image.asset(AppImage.fillbottomnavbarcategoryicon,scale: 1.2,),
+          outlineImage: Image.asset(AppImage.outlinebottomnavbarcategoryicon,scale: 1.2,),
         ),
         BarItem(
-          filledIcon: Icons.folder_rounded,
-          outlinedIcon: Icons.folder_outlined,
-          Text: "",
+          Text: "Cart",
+          fillImage: Image.asset(AppImage.fillbottomnavbarshoppingcarticon,scale: 1.2,),
+          outlineImage: Image.asset(AppImage.outlinebottomnavbarshoppingcarticon,scale: 1.2,),
         ),
-      ], );
+        BarItem(
+          Text: "Wish",
+          fillImage: Image.asset(AppImage.fillbottomnavbarhgiftboxicon,scale: 1.2,),
+          outlineImage:Image.asset(AppImage.outlinebottomnavbarhgiftboxicon,scale: 1.2,),
+        ),
+      ],
+    );
   }
 }
--- lib/app/utility/app_image/app_image.dart
+++ lib/app/utility/app_image/app_image.dart
@@ -2,4 +2,17 @@ class AppImage{
 
   static const String Splashimage = 'assets/splashlogo/logo.svg';
 
+  static const String Bottomnavbaricon = 'assets/bottom_nav_icon/';
+
+  static const String outlinebottomnavbarhomeicon = '${Bottomnavbaricon}/home.png';
+  static const String outlinebottomnavbarcategoryicon = '${Bottomnavbaricon}/category.png';
+  static const String outlinebottomnavbarshoppingcarticon = '${Bottomnavbaricon}/shopping-cart.png';
+  static const String outlinebottomnavbarhgiftboxicon = '${Bottomnavbaricon}/gift-box.png';
+
+  static const String fillbottomnavbarhomeicon = '${Bottomnavbaricon}/homefill.png';
+  static const String fillbottomnavbarcategoryicon = '${Bottomnavbaricon}/categoryfill.png';
+  static const String fillbottomnavbarshoppingcarticon = '${Bottomnavbaricon}/shoppingfill.png';
+  static const String fillbottomnavbarhgiftboxicon = '${Bottomnavbaricon}/giftboxfill.png';
+
+
 }
